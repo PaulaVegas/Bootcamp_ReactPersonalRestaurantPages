1ae543f0ae022b783f3e8f5e55b3bbf143f1fce6
Paula <mv.paula@gmail.com>
2025-07-04 20:14:30 +0200

    project skeleton

---
+++ PersonalRestaurantWebs/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import SunIcon from "./assets/icons/sun.png"
+import MoonIcon from "./assets/icons/moon.png"
+import NavBar from './components/NavBar'
+
+function App() {
+  const [theme, setTheme] = useState('dark') 
+  useEffect(() => {
+    document.body.classList.remove('theme-light', 'theme-dark')
+    document.body.classList.add(`theme-${theme}`)
+  }, [theme])
+
+  const toggleTheme = () => {
+    setTheme(prev => (prev === 'dark' ? 'light' : 'dark'))
+  }
+
+  return (
+    <div className="App">
+      <header>
+        <label className="switch">
+          <input
+            type="checkbox"
+            checked={theme === 'light'}
+            onChange={toggleTheme}
+          />
+          <span className="slider">
+            <span className='icon sun'><img src={SunIcon} className="icon" alt="Sun Icon" /></span>
+            <span className='icon moon'><img src={MoonIcon} className="icon" alt="Moon Icon" /></span>
+          </span>
+        </label>
+      </header>
+      <NavBar />
+    </div>
+  )
+}
+export default App
---
+++ PersonalRestaurantWebs/src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+function NavBar() {
+    return (
+        <nav className="navbar">
+            <h1>Personal and Restaurant</h1>
+        </nav>
+    );
+}
+
+export default NavBar;
